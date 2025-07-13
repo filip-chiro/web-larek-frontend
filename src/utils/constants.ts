f16242a0319e297cc4b63ccb848694a315b4ca8d
Filip Chiro <filip.chiro@yandex.ru>
2025-07-13 17:20:46 +0200

    deploy

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,8 @@
 import { ProductCategory } from "../types";
 
-export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
-export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
+const API_ORIGIN = 'https://larek-api.nomoreparties.com';
+export const API_URL = `${API_ORIGIN}/api/weblarek`;
+export const CDN_URL = `${API_ORIGIN}/content/weblarek`;
 
 export const settings = {
 
